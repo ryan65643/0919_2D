70c5ac76ac18b4290bd74e9cc4be8ef5e7ddb6cb
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-10-31 12:48:26 +0800

    123

--- New Unity Project/Assets/Script/Test.cs
+++ New Unity Project/Assets/Script/Test.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public int tests;
+    public int rrrr { get =>555 ; }
+    public float rrrrr
+    {
+        set
+        {
+            print(value) ;
+        }
+    }
+    private void Start()
+    {
+        tests = 555;
+        print(tests+rrrr);
+        rrrrr = 999;
+    }
+
 
 }
  
