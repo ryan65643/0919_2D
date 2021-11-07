b54f7c23541c23f6d1c2a252f5a2cb46305e6c74
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-11-07 17:01:30 +0800

    23132131

--- New Unity Project/Assets/Script/Test.cs
+++ New Unity Project/Assets/Script/Test.cs
@@ -3,21 +3,56 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public int tests;
-    public int rrrr { get =>555 ; }
+    public int testsd=1;
+    public int rrrr { get => 555; }
     public float rrrrr
     {
         set
         {
-            print(value) ;
+            print(value);
         }
     }
+    public enum fdfgdfgdfg
+    {
+    a,b,c,d,¤¤123¤å
+    }
+
+
+    public fdfgdfgdfg rasd;
     private void Start()
     {
         tests = 555;
-        print(tests+rrrr);
+        print(tests + rrrr);
         rrrrr = 999;
+        dd();
+
+        switch (rasd)
+        {
+            case fdfgdfgdfg.a:
+                break;
+            case fdfgdfgdfg.b:
+                break;
+            case fdfgdfgdfg.c:
+                break;
+            case fdfgdfgdfg.d:
+                break;
+
+        }
+
     }
 
 
-}
+
+
+
+    private void dd()
+    {
+        while (testsd < 6)
+        {
+            print(testsd);
+            testsd++;
+
+        }
+    }
+    }
  
