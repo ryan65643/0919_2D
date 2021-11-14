855a594d3529cc884e241d2880636b5d61642f99
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-11-14 16:57:45 +0800

    ㄕㄨ

---
+++ New Unity Project/Assets/Script/Start.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("¹CÀ¸µe­±");
+    }
+
+
+    public void Setting()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
