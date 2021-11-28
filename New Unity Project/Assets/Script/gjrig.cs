f12f133803799be8387c00028532d210cdefb9d9
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-11-28 16:55:46 +0800

    121

---
+++ New Unity Project/Assets/Script/gjrig.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gjrig : MonoBehaviour
+{
+  private void CheckD()
+    {
+        if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            dircet=Dirce
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+    }
+}
